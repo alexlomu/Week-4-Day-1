7bc28cf57453ff5a0b38846a5b35a05623ebcb1f
alexlomu <91721507+alexlomu@users.noreply.github.com>
2022-12-08 12:24:55 +0100

    preguntas

---
+++ main.py
@@ -0,0 +1,47 @@
+#Import all the necessary libraries here:
+
+#Import the Fitbit2 dataset and store it in a variable called fitbit. You can find the dataset in Ironhack's database:
+
+#How the average number of steps change by month. Use the appropriate visualization to show the median steps by month. Is Fitbitter more active on weekend or workdays? 
+
+#Write a loop to plot 3 scatter plots of the following features:
+
+#Import the titanic dataset and store it in a variable called titanic. You can find the dataset in Ironhack's database:
+
+#Explore the titanic dataset using Pandas dtypes.
+
+#What are your numerical variables? What are your categorical variables?
+
+#Set the plot style to classic and the figure size to (12,6).
+
+#Use the right visulalization to show the distribution of column Age.
+
+#Use subplots and plot the distribution of the Age with bins equal to 10, 20 and 50.
+
+#How does the bin size affect your plot?
+
+
+#Use seaborn to show the distribution of column Age.
+
+#Use the right plot to visualize column Gender. There are 2 ways of doing it. Do it both ways.
+
+#Use the right plot to visualize the column Pclass.
+
+#We would like to have in one single plot the summary statistics of the feature Age. What kind of plot would you use? Plot it.
+
+#What does the last plot tell you about the feature Age?
+
+#Now in addition to the summary statistics, we want to have in the same plot the distribution of Age. What kind of plot would you use? Plot it.
+
+#What additional information does the last plot provide about feature Age?
+
+#We suspect that there is a linear relationship between Fare and Age. Use the right plot to show the relationship between these 2 features. There are 2 ways, please do it both ways.
+
+#Plot the correlation matrix using seaborn.
+
+#What are the most correlated features?
+
+#Use the most appropriate plot to display the summary statistics of Age depending on Pclass.
+
+#Use seaborn to plot the distribution of Age based on the Gender.
+
